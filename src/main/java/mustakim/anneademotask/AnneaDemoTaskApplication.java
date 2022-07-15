0ed2cf2f280a0f1df9b65e7430b438260e1d0fe4
Mustakim Billah <mustakim.billah@revesoft.com>
2022-07-15 12:43:57 +0600

    Project Initialized

---
+++ src/main/java/mustakim/anneademotask/AnneaDemoTaskApplication.java
@@ -0,0 +1,13 @@
+package mustakim.anneademotask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnneaDemoTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnneaDemoTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mustakim/anneademotask/AnneaDemoTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package mustakim.anneademotask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnneaDemoTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
